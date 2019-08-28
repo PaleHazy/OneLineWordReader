5ef18a034e5d7b8ad42afb93c24242ed4ada6476
PaleHazy <austinrpg@live.com>
2019-08-28 16:00:25 -0500

    initial commit

---
+++ reader.js
@@ -0,0 +1,71 @@
+let splitWords;
+let counter = 0;
+let interval;
+let intervalSpeed = 500;
+// ------------------------------------------------------------------------- //
+
+$('#msSpeed').on('input', () => {
+  intervalSpeed = $('#msSpeed').val();
+  console.log('intervalSpeed:', intervalSpeed);
+});
+
+// ------------------------------------------------------------------------- //
+$('#restart').on('click', () => {
+  counter = 0;
+  appender(splitWords[0]);
+});
+// ------------------------------------------------------------------------- //
+
+$('textarea').on('input', () => {
+  splitter();
+  console.log(splitWords);
+});
+
+function getInput() {
+  let textData = $('#textInput').val();
+  return textData;
+}
+function splitter() {
+  splitWords = getInput().split(/\s|\n|\t|\r/);
+}
+
+// ------------------------------------------------------------------------- //
+
+$('#runButton').on('click', runAPP);
+
+function appender(x) {
+  $('#outputText').text(x);
+  console.log('running bihh im appender');
+}
+
+function current() {
+  let currentWord = splitWords[counter];
+  return currentWord;
+}
+function mix() {
+  if (counter < splitWords.length) {
+    appender(current());
+    counter++;
+  } else {
+    clearInterval(interval);
+  }
+}
+
+function runAPP() {
+  interval = setInterval(mix, intervalSpeed);
+}
+// ------------------------------------------------------------------------- //
+
+$('#stopButton').on('click', () => {
+  clearInterval(interval);
+});
+// ------------------------------------------------------------------------- //
+$('#back').on('click', () => {
+  counter = counter - 15;
+  appender(splitWords[counter]);
+});
+// ------------------------------------------------------------------------- //
+$('#skip').on('click', () => {
+  counter = counter + 15;
+  appender(splitWords[counter]);
+});
